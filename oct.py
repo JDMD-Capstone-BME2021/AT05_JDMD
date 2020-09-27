a57918507a1d7131e9759ce3d9e4fbcc0e08bf1d
DShuraev <dshuraev@users.noreply.github.com>
2020-09-27 06:06:34 -0400

    Added docstrings

--- oct.py
+++ oct.py
@@ -6,6 +6,13 @@ from skimage.transform import rescale, iradon, iradon_sart
 
 
 def _make_square(im: Image, minsize: int = 256, fill_color: tuple = (255, 255, 255, 0)) -> np.array:
+    """
+Creates a sqaure image of the specified size with other image pasted in center
+    :param im: image to paste
+    :param minsize: minimum image size
+    :param fill_color: image fill color
+    :return: PIL image
+    """
     x, y = im.size
     size = max(minsize, x, y)
     new_im = Image.new('RGBA', (size, size), fill_color)
@@ -14,6 +21,13 @@ def _make_square(im: Image, minsize: int = 256, fill_color: tuple = (255, 255, 2
 
 
 def _prepare_img(img_path: Path, resolution: int = 256, padding: float = 0.3) -> np.array:
+    """
+Preprocesses the image -- loads it, makes it square, converts to greyscale, inverts, adds padding and rescales
+    :param img_path: path to the image to load
+    :param resolution: final resolution of the image
+    :param padding: padding fraction
+    :return: processed image as a np.array
+    """
     src = Image.open(img_path)
     img = _make_square(src, minsize=int(src.size[0] * (1 + padding)))
     img = ImageOps.grayscale(img)
@@ -23,6 +37,22 @@ def _prepare_img(img_path: Path, resolution: int = 256, padding: float = 0.3) ->
 
 
 def load_images(dir_path: str, resolution: int = 256, padding: float = 0.3) -> np.array:
+    """
+Loads images from specified directory and preprocesses them:
+
+- makes it square
+
+- converts to greyscale & inverts it
+
+- adds padding
+
+- rescales it
+
+    :param dir_path: directory to load images from
+    :param resolution: final image resolution
+    :param padding: padding fraction
+    :return: array of images, with image index in position 2
+    """
     files = [x for x in Path(dir_path).rglob('*')]
     n = len(files)
     images = np.zeros((resolution, resolution, n))
@@ -34,6 +64,11 @@ def load_images(dir_path: str, resolution: int = 256, padding: float = 0.3) -> n
 
 
 def make_sinogram(images: np.array) -> np.array:
+    """
+Makes a sinogram from an image array
+    :param images: image array with image index in position 2
+    :return: array of sinogram with sinogram index in position 0
+    """
     print('Constructing sinogram\n')
     sinogram = np.zeros(images.shape)
     for i in range(sinogram.shape[0]):
@@ -42,6 +77,14 @@ def make_sinogram(images: np.array) -> np.array:
 
 
 def reconstruct_fbp(sinogram: np.array, nthreads: int = 1, method: str = 'fbp', **kwargs) -> np.array:
+    """
+Reconstructs tomographic images from a sinogram array
+    :param sinogram: sinogram array, indexed at position 0
+    :param nthreads: number of threads
+    :param method: reconstruction method -- 'fbp' (filtered back projection) or 'sart' simultaneous reconstruction
+    :param kwargs: arguments forwarded to reconstuction method
+    :return: tomographic image todo: add specification to axes
+    """
     reconstructed = np.zeros((sinogram.shape[0], sinogram.shape[0], sinogram.shape[0]))
     lock = threading.Lock()
 
